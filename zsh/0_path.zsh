9c23a9c12a7f6aa4d73ccbf9e12ee5140c82122e
Matt Kunze <matt.kunze@datasplice.com>
2013-04-26 13:02:16 -0500

    add alias to edit hosts file

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,2 +1,2 @@
 # path, the 0 in the filename causes this to load first
-export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr:$HOME/bin:/usr/local/share/npm/bin
+export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr:$HOME/bin:/usr/local/share/npm/bin:$HOME/Frameworks/adt-bundle-mac-x86_64-20130522/sdk/platform-tools:$HOME/Frameworks/adt-bundle-mac-x86_64-20130522/sdk/tools

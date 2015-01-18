723c5a04894736f09864b781b1c5a04942eabf80
Matt Kunze <matt.kunze@datasplice.com>
2015-01-18 15:10:09 -0700

    add android sdk to path

--- zsh/0000_before.zsh
+++ zsh/0000_before.zsh
@@ -5,3 +5,5 @@ if [ -d $HOME/.zsh.before/ ]; then
     for config_file ($HOME/.zsh.before/*.zsh) source $config_file
   fi
 fi
+
+export ANDROID_HOME=~/Frameworks/android-sdk-macosx
--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,7 +1,11 @@
 # path, the 0 in the filename causes this to load first
 path=(
   $path
+  $HOME/bin
   $HOME/.yadr/bin
   $HOME/.yadr/bin/yadr
+  $ANDROID_HOME/tools
+  $ANDROID_HOME/platform-tools
+  $ANDROID_HOME/build-tools
   /usr/local/share/npm/bin
 )
